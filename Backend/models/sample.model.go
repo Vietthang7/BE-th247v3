90a962a99561ea2e5cae56b63fae0a8b0bec8900
vietthang7 <nguyenvietthang12092004@gmail.com>
2025-01-15 13:44:33 +0700

    update code

---
+++ Backend/models/sample.model.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+	"time"
+)
+
+type Model struct {
+	ID        uuid.UUID  `gorm:"primaryKey;default:(UUID())" json:"id,omitempty"`
+	CreatedAt *time.Time `gorm:"index" json:"created_at,omitempty"`
+	UpdatedAt *time.Time `json:"updated_at,omitempty"`
+	DeletedAt *time.Time `json:"deleted_at,omitempty"`
+}
+
+func (u *Model) BeforeCreate(*gorm.DB) (err error) {
+	if u.ID == uuid.Nil {
+		u.ID = uuid.New()
+	}
+	return
+}
