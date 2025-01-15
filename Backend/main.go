00f1a8c9214ee1d91ee5196f0f29b105e901046b
vietthang7 <nguyenvietthang12092004@gmail.com>
2025-01-15 13:44:33 +0700

    update code

--- Backend/main.go
+++ Backend/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"intern_247/config"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	config.ConnectDB()
 	app := fiber.New()
 	app.Get("/", func(c *fiber.Ctx) error {
 		fmt.Println("ok")
