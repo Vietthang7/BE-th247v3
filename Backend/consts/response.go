aa3c6f6aa1222e6e485133211391937158ce623e
NBK1612 <baokhanh16122003@gmail.com>
2025-02-06 15:01:53 +0700

    .

--- Backend/consts/response.go
+++ Backend/consts/response.go
@@ -8,12 +8,6 @@ const (
 	CreateSuccess = "Tạo dữ liệu thành công!"
 	GetFail       = "Lấy dữ liệu lỗi"
 	DataExists    = "Dữ liệu đã tồn tại!"
-<<<<<<< HEAD
-
-	GetSuccess    = "Lấy dữ liệu thành công!"
 	UpdateSuccess = "Cập nhật thành công!"
-=======
-	UpdateSuccess = "Cập nhật dữ liệu thành công!"
 	UpdateFail    = "Cập nhật dữ liệu lỗi"
->>>>>>> e01c888beb3785c50fba20ec84080d5831f36a03
 )
