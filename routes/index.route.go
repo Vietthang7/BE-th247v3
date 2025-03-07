1c99d280a4f937de0cdc1278fa90da74fdf3d63e
thangnguyen <nguyenvietthang12092004@gmail.com>
2025-03-07 15:29:55 +0700

    update

--- routes/index.route.go
+++ routes/index.route.go
@@ -8,15 +8,13 @@ import (
 )
 
 func AdminRoutes(app *fiber.App) {
+	app.Use(cors.New())
 	// Define a group for admin routes
 	admin := app.Group("/api/admin")
 	admin.Use(logger.New())
 	admin.Use(mdw.AdminAuthentication)
 	//admin := app.Group("/api/admin", mdw.AdminAuthentication)
 	auth := app.Group("/api/auth")
-
-	// Call CenterRouter to register its routes
-	app.Use(cors.New())
 	CenterRouter(admin)
 	AuthRoute(auth)
 	StudentNeedsRoute(admin)
